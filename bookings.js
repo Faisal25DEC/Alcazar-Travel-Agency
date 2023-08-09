ac40f850e240655fa1f9acdc94fa491b9d6801b3
shubham007x <shubham.agdari@gmail.com>
2023-08-10 03:26:57 +0530

    added details page in right canvas

--- bookings.js
+++ bookings.js
@@ -33,7 +33,7 @@ var bookings = [
         info: "It has been an important historical part of Port Blair. Notable freedom fighters such as Veer Savarkar, Yogendra Shukla, Batukeshwar Dutt, and Babarao Savarkar were some of the inmates here. Don’t miss the light and sound show(Monday, Wednesday and Friday) when you visit Cellular jail. ",
         images: "https://www.holidify.com/images/cmsuploads/compressed/3616_20190213160612jpg",
         location: "https://www.google.co.in/maps/place/Cellular+Jail+National+Monument/@11.6738247,92.7479768,15z/data=!4m2!3m1!1s0x0:0x616a8c6623fdba3f?sa=X&ved=2ahUKEwihqtPEuvPlAhU4IbcAHThdBsAQ_BIwJnoECA4QCA",
-        id: 2,
+        id: 3,
         price: "676.9",
         ratings: "1.6",
         recommended: true,
@@ -44,50 +44,60 @@ var bookings = [
     },]
 var close = [
     {
-        "name": "SAMUDRIKA NAVAL",
-        "info": "This museum is a perfect blend of historical and modern Andaman. A massive blue whale, in its skeletal form, of course, greets you as you enter the museum. There is also an aquarium with fish of all shapes and sizes, from a parrotfish to the rare and venomous species of stonefish and corals.",
-        "images": "https://www.tripsavvy.com/thmb/SF7NoKaUPvXKxBkDbmPrC-GSddU=/2121x1414/filters:fill(auto,1)/GettyImages-508601155-592e840f5f9b5859500d0724.jpg",
-        "location": "https://www.google.co.in/maps/place/Samudrika+Marine+Museum/@11.6718057,92.7263692,15z/data=!4m2!3m1!1s0x0:0xd7352994a1c32fb8?sa=X&ved=2ahUKEwi64_yEu_PlAhWMILcAHYtnBf0Q_BIwC3oECBAQCA",
-        "id": 4,
-        "price": "893.9",
-        "ratings": "2.8",
-        "recommended": false,
-        "trending": false,
+        name: "SAMUDRIKA NAVAL",
+        info: "This museum is a perfect blend of historical and modern Andaman. A massive blue whale, in its skeletal form, of course, greets you as you enter the museum. There is also an aquarium with fish of all shapes and sizes, from a parrotfish to the rare and venomous species of stonefish and corals.",
+        images: "https://www.tripsavvy.com/thmb/SF7NoKaUPvXKxBkDbmPrC-GSddU=/2121x1414/filters:fill(auto,1)/GettyImages-508601155-592e840f5f9b5859500d0724.jpg",
+        location: "https://www.google.co.in/maps/place/Samudrika+Marine+Museum/@11.6718057,92.7263692,15z/data=!4m2!3m1!1s0x0:0xd7352994a1c32fb8?sa=X&ved=2ahUKEwi64_yEu_PlAhWMILcAHYtnBf0Q_BIwC3oECBAQCA",
+        id:4,
+        price: "893.9",
+        ratings: "2.8",
+        recommended: false,
+        trending: false,
         sdate: "2-Aug",
         edate: "5-Aug",
         guest: "4",
     },
     {
-        "name": "HAVELOCK ISLAND",
-        "info": "Havelock Island is part of Ritchie’s Archipelago, in India’s Andaman Islands. It’s known for its dive sites and beaches, like Elephant Beach, with its coral reefs. Crescent-shaped Radhanagar Beach is a popular spot for watching the sunset. On the island’s east side, rocky sections mark long, tree-lined Vijaynagar Beach.",
-        "images": "https://www.tripsavvy.com/thmb/SF7NoKaUPvXKxBkDbmPrC-GSddU=/2121x1414/filters:fill(auto,1)/GettyImages-508601155-592e840f5f9b5859500d0724.jpg",
-        "location": "https://www.google.co.in/maps/place/Swaraj+Dweep/@11.9656084,92.9194242,12z/data=!3m1!4b1!4m5!3m4!1s0x3088d3d85e0fe039:0x25c8aaaa513ef4bf!8m2!3d11.9760503!4d92.9875565",
-        "id": 5,
-        "price": "513.3",
+        name: "HAVELOCK ISLAND",
+        info: "Havelock Island is part of Ritchie’s Archipelago, in India’s Andaman Islands. It’s known for its dive sites and beaches, like Elephant Beach, with its coral reefs. Crescent-shaped Radhanagar Beach is a popular spot for watching the sunset. On the island’s east side, rocky sections mark long, tree-lined Vijaynagar Beach.",
+        images: "https://www.tripsavvy.com/thmb/SF7NoKaUPvXKxBkDbmPrC-GSddU=/2121x1414/filters:fill(auto,1)/GettyImages-508601155-592e840f5f9b5859500d0724.jpg",
+        location: "https://www.google.co.in/maps/place/Swaraj+Dweep/@11.9656084,92.9194242,12z/data=!3m1!4b1!4m5!3m4!1s0x3088d3d85e0fe039:0x25c8aaaa513ef4bf!8m2!3d11.9760503!4d92.9875565",
+        id: 5,
+        price: "513.3",
         ratings: "3.2",
-        "recommended": false,
+        recommended: false,
         trending: false,
         sdate: "2-Aug",
         edate: "5-Aug",
         guest: "4",
     }]
+
+    localStorage.setItem("bookings",JSON.stringify(bookings));
+    localStorage.setItem("past",JSON.stringify(close));
 document.querySelector("#btnradio1").addEventListener("click", function () {
-    updateDisplay(bookings);
+    var book=JSON.parse(localStorage.getItem("bookings"));
+    if(book.length==0){
+        document.querySelector("#content").innerHTML=`<h2>No Current Bookings Available<h2/>`;
+    }else updateDisplay(book);
    
 })
 document.querySelector("#btnradio2").addEventListener("click", function () {
-    updateDisplay(close);
+    var closed=JSON.parse(localStorage.getItem("past"));
+    updateDisplay(closed);
     var ratingsElements = document.querySelectorAll(".ratings");
     ratingsElements.forEach(function (element) {
         element.style.display = "block";
     });
-
+    var r = document.querySelectorAll(".cancel");
+    r.forEach(function (element) {
+        element.style.display = "none";
+    });
 })
 var main = document.querySelector("#content");
 function updateDisplay(arr) {
     main.innerHTML = "";
-    arr.forEach(element => {
-        console.log(element);
+    arr.forEach((element,index)=> {
+        //console.log(element);
         var card = document.createElement("div");
         card.setAttribute("class", "cr")
         var img = document.createElement("img");
@@ -104,10 +114,12 @@ function updateDisplay(arr) {
 
         details.setAttribute("class", "b")
         details.setAttribute("id","openModalBtn")
+        details.setAttribute("data-bs-toggle","offcanvas")
+        details.setAttribute("data-bs-target","#offcanvasRight")
         details.innerText = "Details";
         var cancel = document.createElement("button");
         cancel.innerText = "Cancel";
-        cancel.setAttribute("class", "b");
+        cancel.setAttribute("class","cancel");
         bt_div.append(details, cancel);
         var rev = document.createElement("div");
         rev.innerHTML = "Ratings :-  "
@@ -116,17 +128,32 @@ function updateDisplay(arr) {
         review(rev);
         var price = document.createElement("h4");
         price.innerText = "Amount: Rs. " + element.price;
-
-
+        cancel.addEventListener("click",function(){
+            console.log("hio")
+            arr.splice(index,1);
+            updateDisplay(arr);
+            localStorage.setItem("bookings",JSON.stringify(arr));
+            if(book.length==0){
+                document.querySelector("#content").innerHTML=`<h2>No Current Bookings Available<h2/>`;
+            }else updateDisplay(book);
+        
+        });
+        
+        
         details.addEventListener("click",function(){
-            console.log(element);
-        var title=document.querySelector("#modal-title");
-        title.innerHTML="";
-        title.innerText=element.name;
-        var info=document.getElementById("modal-body");
-        info.innerHTML="";
-        info.innerText=`${element.info} \n \n Ratings:- ${element.ratings}`;
-        $('#myModal').modal('show'); // Show the modal
+        console.log(element);
+       
+         var title=document.querySelector(".offcanvas-title");
+         title.innerHTML="";
+         title.innerText=element.name;
+         var info=document.querySelector(".offcanvas-body");
+         info.innerHTML="";
+         info.innerText=`${element.info} \n \n Ratings:- ${element.ratings} \n Price: ${element.price} \n Guests: ---`;
+    
+    
+         const targetButton = document.getElementById('openModalBtn');
+         console.log(targetButton);
+
         })
         card.append(img, name, price, guest, p, bt_div, rev);
         main.append(card);
@@ -159,8 +186,14 @@ function review(rev) {
     }
     return rev;
 }
-updateDisplay(bookings);
-// console.log(bookings);
+var book=JSON.parse(localStorage.getItem("bookings"));
+if(book.length==0){
+    document.querySelector("#content").innerHTML=`<h2>No Current Bookings Available<h2/>`;
+}else updateDisplay(book);
+
+
+//console.log(Date.now())
+
 
 // Open the modal
 
