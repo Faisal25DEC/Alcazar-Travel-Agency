bedaf8e7aefdf5796bd5f6fbff038d38dff701ef
shubham007x <shubham.agdari@gmail.com>
2023-08-13 19:10:48 +0530

    merge

---
+++ navbar_2.js
@@ -0,0 +1,34 @@
+function navbar_2() {
+  return `
+  <div class="nav-logo">
+  <img src="./logo.png" alt="" />
+  <p>Alcazar</p>
+</div>
+<div class="nav-search">
+  <div class="input-search">
+    <input id="search" type="text" placeholder="Search for Places" />
+    <div>
+      <i class="fa-solid fa-magnifying-glass"></i>
+    </div>
+  </div>
+  <div class="search-suggestions"></div>
+</div>
+<div class="nav-right">
+  <a>Home</a>
+  <a href = './product.html'>Explore Places</a>
+</div>
+<div class="nav-login-signup-container">
+  <div class="nav-login-signup">
+    <button id="nav-login">Login</button>
+
+    <button id="nav-signup">SignUp</button>
+  
+  </div>
+  <div class="dropdown">
+    <p id = 'login-email'><i class="fa-regular fa-envelope"></i>Login With Email</p>
+    <p id = 'login-google'><i class="fa-brands fa-google"></i>Login With Google</p>
+  </div>
+</div>
+ `;
+}
+export default navbar_2;
